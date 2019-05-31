ee0931c26c4eaef876b966b8727908fd1fd375fb
Carson Sievert <cpsievert1@gmail.com>
2019-05-31 17:48:16 -0500

    fix typo

--- R/imports.R
+++ R/imports.R
@@ -2,5 +2,5 @@
 #' @importFrom sourcetools tokenize_string
 #' @importFrom shiny reactive getDefaultReactiveDomain isolate
 #' @importFrom utils head
-#' @importFrom rlang expr exprs quo enquo eval_tidy new_quosure quo_get_expr %||% is_syntatic_literal
+#' @importFrom rlang expr exprs quo enquo eval_tidy new_quosure quo_get_expr %||% is_syntactic_literal
 NULL
