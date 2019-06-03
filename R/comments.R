d4a512b4e871e1945d9f513d574e39444673871c
Carson Sievert <cpsievert1@gmail.com>
2019-06-03 10:58:21 -0500

    comment must appear as a direct child of a  call; unescape escaping that happens when deparsing a string

--- R/comments.R
+++ R/comments.R
@@ -1,9 +1,8 @@
-# Derived from Hadley Wickham's AST walking helpers
+# From Hadley Wickham's AST walking helpers
 # https://adv-r.hadley.nz/expressions.html#ast-funs
-
 expr_type <- function(x) {
   if (rlang::is_syntactic_literal(x)) {
-    if (is_comment(x)) "comment" else "constant"
+    "constant"
   } else if (is.symbol(x)) {
     "symbol"
   } else if (is.call(x)) {
@@ -15,33 +14,43 @@ expr_type <- function(x) {
   }
 }
 
-is_comment <- function(x) {
-  if (!is.character(x) || length(x) > 1) return(FALSE)
-  grepl("^#", x)
-}
-
 switch_expr <- function(x, ...) {
   switch(expr_type(x), ...) %||% x
 }
 
 comment_identifier_add <- function(x) {
-  # if a 'comment-like' string appears as the last child of a `{` call,
-  # it might be an assignment value, so we throw a warning if that occurs
-  # and add a special class to the string so that when we arrive at the string
-  # in the future, we know not to add the special comment identifier
-  if (length(x) > 1 && identical(x[[1]], quote(`{`)) && is_comment(x[[length(x)]])) {
-    warning("A shinymeta comment can not appear as the last child of a `{` call")
-    x[[length(x)]] <- prefix_class(x[[length(x)]], "lastChildComment")
+
+  if (length(x) > 1 && identical(x[[1]], quote(`{`))) {
+
+    # comment must appear as a direct child of a `{` call
+    x[-1] <- lapply(x[-1], function(y) {
+      if (is_comment(y)) y <- prefix_class(y, "isComment")
+      y
+    })
+
+    # if the comment appears as the last child of a `{` call,
+    # it might be an assignment value, so we throw a warning if that occurs
+    # and add a special class to the string so that when we arrive at the string
+    # in the future, we know not to add the special comment identifier
+    if (is_comment(x[[length(x)]])) {
+      warning("A shinymeta comment can not appear as the last child of a `{` call")
+      x[[length(x)]] <- remove_class(x[[length(x)]], "isComment")
+    }
+
   }
 
   switch_expr(x,
-    comment = if (inherits(x, "lastChildComment")) remove_class(x, "lastChildComment") else paste0(comment_start, x, comment_end),
+    constant = if (inherits(x, "isComment")) paste0(comment_start, x, comment_end) else x,
     # Recursive cases
     call = as.call(lapply(x, comment_identifier_add)),
     pairlist = as.pairlist(lapply(x, comment_identifier_add))
   )
 }
 
+is_comment <- function(x) {
+  if (!is.character(x) || length(x) > 1) return(FALSE)
+  grepl("^#", x)
+}
 
 comment_start <- "######StartOfShinyMetaCommentIdentifier######"
 comment_end <- "######EndOfShinyMetaCommentIdentifier######"
