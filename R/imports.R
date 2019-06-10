9b9684452cd0f40ee3eb403deabe7b0bb5981803
Carson Sievert <cpsievert1@gmail.com>
2019-06-10 14:31:36 -0500

    add fs, callr to imports; un-export constf (fixes #17)

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,6 @@
 #' @importFrom styler style_text
 #' @importFrom sourcetools tokenize_string
 #' @importFrom shiny reactive getDefaultReactiveDomain isolate
-#' @importFrom utils head
+#' @importFrom utils head zip
 #' @importFrom rlang expr exprs quo enquo eval_tidy new_quosure quo_get_expr %||% is_syntactic_literal
 NULL
--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,3 @@
-#' @export
 constf <- function(value) {
   force(value)
   function() {
