c1e01503f4afaecf03e00347aae1205d738c73fd
Carson <cpsievert1@gmail.com>
2019-07-10 15:42:38 -0500

    give each example app a proper home

---
+++ inst/examples/anova/app.R
@@ -0,0 +1,2 @@
+# https://github.com/cpsievert/DIY_ANOVA
+shiny::runGitHub("DIY_ANOVA", "cpsievert")
---
+++ inst/examples/cranview/app.R
@@ -0,0 +1,2 @@
+# https://github.com/cpsievert/cranview
+shiny::runGitHub("cranview", "cpsievert")
