adf54f65bce3cf194ef5c7744ece5050c0ce04bc
Joe Cheng <joe@rstudio.com>
2019-08-02 11:57:16 -0700

    Warn users at onLoad about upcoming syntax changes

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,8 @@
 .onLoad <- function(libname, pkgname) {
   # Turn on this top secret feature of Shiny! Shhhhh!
   options(shiny.allowoutputreads = TRUE)
+
+  warning(call. = FALSE, immediate. = TRUE,
+    "Thanks for trying out shinymeta! This package is currently\n*experimental* and will definitely have breaking changes in the near\nfuture; please use caution, especially when upgrading to a newer\nshinymeta build. https://github.com/rstudio/shinymeta/blob/master/NEWS.md"
+  )
 }
