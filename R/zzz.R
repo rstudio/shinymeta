50b8213e46a0746ebcf8aee5c23113cadb3af5a3
Carson Sievert <cpsievert1@gmail.com>
2019-08-02 14:05:40 -0500

    message instead of warning
    The warning would otherwise break the Travis build

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
   # Turn on this top secret feature of Shiny! Shhhhh!
   options(shiny.allowoutputreads = TRUE)
 
-  warning(call. = FALSE, immediate. = TRUE,
+  message(
     "Thanks for trying out shinymeta! This package is currently\n*experimental* and will definitely have breaking changes in the near\nfuture; please use caution, especially when upgrading to a newer\nshinymeta build. https://github.com/rstudio/shinymeta/blob/master/NEWS.md"
   )
 }
