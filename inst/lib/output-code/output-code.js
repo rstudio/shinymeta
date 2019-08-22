5b1fe550ed4ddf4205970c5b1680e5cafaa554f6
Carson <cpsievert1@gmail.com>
2019-08-22 17:44:30 -0400

    Set input value when outputCodeButton()'s button is clicked (not it's container), fixes #60

--- inst/lib/output-code/output-code.js
+++ inst/lib/output-code/output-code.js
@@ -1,9 +1,9 @@
 $(function () {
-  $(".shinymeta-output-code").click(function() {
+  $(".shinymeta-output-code button").click(function() {
     var $el = $(this);
     var val = $el.data('val') || 0;
     $el.data('val', val + 1);
-    var output_id = $el.find(".shiny-bound-output").attr("id");
+    var output_id = $(".shinymeta-output-code").find(".shiny-bound-output").attr("id");
     var event_id = output_id + "_output_code";
     Shiny.onInputChange(event_id, $el.data('val'));
   });
