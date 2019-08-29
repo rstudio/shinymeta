f39ae3ec0fad37c2e8daec618745784c87c64f0f
Carson <cpsievert1@gmail.com>
2019-08-29 13:31:43 -0500

    Manually register knit_print method onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -10,4 +10,28 @@
     "* build. https://github.com/rstudio/shinymeta/blob/master/NEWS.md  *\n",
     "********************************************************************"
   )
+
+  registerMethods(list(
+    # c(package, genname, class)
+    c("knitr", "knit_print", "shinyMetaExpr")
+  ))
+}
+
+# https://github.com/rstudio/htmltools/blob/cb452a837/R/tags.R#L22-L57
+registerMethods <- function(methods) {
+  lapply(methods, function(method) {
+    pkg <- method[[1]]
+    generic <- method[[2]]
+    class <- method[[3]]
+    func <- get(paste(generic, class, sep="."))
+    if (pkg %in% loadedNamespaces()) {
+      registerS3method(generic, class, func, envir = asNamespace(pkg))
+    }
+    setHook(
+      packageEvent(pkg, "onLoad"),
+      function(...) {
+        registerS3method(generic, class, func, envir = asNamespace(pkg))
+      }
+    )
+  })
 }
