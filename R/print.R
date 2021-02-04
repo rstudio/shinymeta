a85f7e9852bc484418c90c463020990f677a65b8
Carson <cpsievert1@gmail.com>
2021-02-04 13:12:38 -0600

    Add Rd page for knit_print.shinyMetaExpr

--- R/print.R
+++ R/print.R
@@ -18,6 +18,13 @@ format.shinyMetaExpr <- function(x, ...) {
   format(deparseCode(x), ...)
 }
 
+#' Knitr S3 methods
+#'
+#' This S3 method allows [metaExpr()]s to print themselves in
+#' knitr/rmarkdown documents.
+#'
+#' @param x Object to knit_print
+#' @param ... Additional knit_print arguments
 #' @export
 knit_print.shinyMetaExpr <- function(x, ...) {
   deparseCode(x)
