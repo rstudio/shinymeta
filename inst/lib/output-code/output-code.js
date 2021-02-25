565bc166898a51db33c3e09ff633ec36bfc4207b
Carson <cpsievert1@gmail.com>
2021-02-25 11:57:57 -0600

    Code review; simplify; add BS4 support

--- inst/lib/output-code/output-code.js
+++ inst/lib/output-code/output-code.js
@@ -1,10 +1,8 @@
-$(function () {
+$(document).on("click", ".shinymeta-output-code button", function() {
+  var id = $(this).parents(".shinymeta-output-code").find(".shiny-bound-output").attr("id");
   // Invalidate `input$id_output_code` whenever the button of
   // shinymeta::outputCodeButton(plotOutput("id")) is pressed
-  $(".shinymeta-output-code button").click(function() {
-    var id = $(this).parents(".shinymeta-output-code").find(".shiny-bound-output").attr("id");
-    if (id) {
-      Shiny.setInputValue(id + "_output_code", 'true', {priority: 'event'});
-    }
-  });
+  if (id) {
+    Shiny.setInputValue(id + "_output_code", 'true', {priority: 'event'});
+  }
 });
