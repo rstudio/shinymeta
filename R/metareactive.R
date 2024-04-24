bb93c1e0c962b5488c990d9b982a8ef7f950f428
Joe Cheng <joe@posit.co>
2024-04-24 09:21:42 -0700

    Give metaReactive a "reactive" class for is.reactive()

--- R/metareactive.R
+++ R/metareactive.R
@@ -173,7 +173,7 @@ metaReactiveImpl <- function(expr, env, varname, domain, inline) {
         }
       )
     },
-    class = c("shinymeta_reactive", "shinymeta_object", "function"),
+    class = c("shinymeta_reactive", "shinymeta_object", "reactive", "function"),
     shinymetaVarname = varname,
     shinymetaUID = getFromNamespace("createUniqueId", "shiny")(8),
     shinymetaDomain = domain,
